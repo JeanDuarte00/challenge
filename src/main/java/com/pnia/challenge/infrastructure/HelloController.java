538938644b6a2af518711a81ef1457dea4991703
jean.b <jean.b@dmz.org.br>
2021-11-11 02:27:48 -0300

    refactoring testing for endpoint

--- src/main/java/com/pnia/challenge/infrastructure/HelloController.java
+++ src/main/java/com/pnia/challenge/infrastructure/HelloController.java
@@ -1,23 +1,35 @@
 package com.pnia.challenge.infrastructure;
 
+import com.pnia.challenge.domain.entities.BusinessSector;
 import com.pnia.challenge.domain.enuns.BusinessSectorType;
 import com.pnia.challenge.infrastructure.HttpClient.TalkDeskBusinessSectorAPI;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(value = "Question")
+@Api(value = "Tests")
 @RestController
 public class HelloController {
 
+    @Autowired
     private TalkDeskBusinessSectorAPI endpoint;
 
-    @GetMapping("/")
-    @ApiOperation(value = "Mostra lista de questões")
+    @GetMapping("/swagger")
     public String index() {
-        var resp = "health check => http://localhost:8080/swagger-ui.html#/";//endpoint.getSector("1");
-        return "Greetings from Spring Boot! : " + resp ;
+        return "http://localhost:8080/swagger-ui.html#/" ;
     }
 
+    @GetMapping("/ping")
+    public String healthCheck() {
+        return "pong";
+    }
+    @GetMapping("/call/{number}")
+    public BusinessSector callTDApi(@PathVariable("number") final String number) {
+        String path = "${talkdesk.businessSector.api} + ${talkdesk.businessSector.endpoint.sector}";
+        var resp = endpoint.getSector(number).getBody();
+        return resp;
+    }
 }
--- src/main/java/com/pnia/challenge/infrastructure/HttpClient/TalkDeskBusinessSectorAPI.java
+++ src/main/java/com/pnia/challenge/infrastructure/HttpClient/TalkDeskBusinessSectorAPI.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "${talkdesk.businessSector.api} + ${talkdesk.businessSector.endpoint.sector}", name = "sector")
+@FeignClient(url = "${talkdesk.businessSector.api}${talkdesk.businessSector.endpoint.sector}", name = "sector")
 public interface TalkDeskBusinessSectorAPI {
 
     @GetMapping("/{number}")
